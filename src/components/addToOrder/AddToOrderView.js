9a8535568e2a0bf61e90b2557c04c3c8a6c13635
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-02-14 00:33:30 +1300

    Bug fixed

--- src/components/addToOrder/AddToOrderView.js
+++ src/components/addToOrder/AddToOrderView.js
@@ -1,34 +1,21 @@
 // @flow
 
 import { TouchableItem } from '@microbusiness/common-react-native';
-import React, { Component } from 'react';
+import React from 'react';
 import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 import Styles from './Styles';
 
-class AddToOrderView extends Component {
-  onUpdateOrder = () => {
-    this.props.handleSubmit(this.props.updateOrderPressed);
-  };
-
-  onAddOrder = () => {
-    this.props.handleSubmit(this.props.addToOrderPressed);
-  };
-
-  render = () => {
-    const { isUpdatingOrder, orderQuantity } = this.props;
-
-    return isUpdatingOrder ? (
-      <TouchableItem onPress={this.onUpdateOrder} style={Styles.container}>
-        <Text style={Styles.text}>Update Order</Text>
-      </TouchableItem>
-    ) : (
-      <TouchableItem onPress={this.onAddOrder} style={Styles.container}>
-        <Text style={Styles.text}>ADD {orderQuantity} TO ORDER</Text>
-      </TouchableItem>
-    );
-  };
-}
+const AddToOrderView = props =>
+  props.isUpdatingOrder ? (
+    <TouchableItem onPress={props.handleSubmit(props.updateOrderPressed)} style={Styles.container}>
+      <Text style={Styles.text}>Update Order</Text>
+    </TouchableItem>
+  ) : (
+    <TouchableItem onPress={props.handleSubmit(props.addToOrderPressed)} style={Styles.container}>
+      <Text style={Styles.text}>ADD {props.orderQuantity} TO ORDER</Text>
+    </TouchableItem>
+  );
 
 AddToOrderView.propTypes = {
   addToOrderPressed: PropTypes.func.isRequired,
