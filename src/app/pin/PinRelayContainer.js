c1aad6c3d660ce1af09fcb1aaf44df414480ba4e
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-02-18 19:47:11 +1300

    Added coument templtes to pin relay container

--- src/app/pin/PinRelayContainer.js
+++ src/app/pin/PinRelayContainer.js
@@ -32,6 +32,10 @@ export default createPaginationContainer(
                   primaryTopBannerImageUrl
                   secondaryTopBannerImageUrl
                 }
+                documentTemplates {
+                  name
+                  template
+                }
               }
             }
           }
