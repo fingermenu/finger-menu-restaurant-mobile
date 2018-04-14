319229f09e6ce0692203e14c379e8cf8b869a3f1
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-04-14 18:56:09 +1200

    Fixed bug in give to geust in table detail screen

--- src/app/tableDetail/TableDetailRelayContainer.js
+++ src/app/tableDetail/TableDetailRelayContainer.js
@@ -11,6 +11,7 @@ export default createRefetchContainer(
         id
         table(tableId: $tableIdForTableQuery) {
           id
+          lastOrderCorrelationId
           name
           customerName
           notes
