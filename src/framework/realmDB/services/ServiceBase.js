5ad34e194e8abd644060d1f7b5792350424a3492
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-05-08 17:38:15 +1200

    Added query builder functions to Realm ServiceBase

--- src/framework/realmDB/services/ServiceBase.js
+++ src/framework/realmDB/services/ServiceBase.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { Common } from '@microbusiness/common-javascript';
 import Immutable from 'immutable';
 import cuid from 'cuid';
 import Query from './Query';
@@ -25,6 +26,124 @@ export default class ServiceBase {
         const innerQuery = objectIds.reduce((reduction, objectId) => reduction.addOrQuery(`id = "${objectId}"`), new Query()).getQuery();
 
         query.addAndQuery(`(${innerQuery})`);
+      }
+    }
+
+    return query;
+  };
+
+  static addStringQuery = (conditions, query, conditionPropKey, columnName) => {
+    if (conditions.has(conditionPropKey)) {
+      const value = conditions.get(conditionPropKey);
+
+      if (Common.isNotUndefined(value)) {
+        query.addAndQuery(`${columnName} = "${value}"`);
+      }
+    }
+
+    if (conditions.has(`startsWith_${conditionPropKey}`)) {
+      const value = conditions.get(`startsWith_${conditionPropKey}`);
+
+      if (Common.isNotUndefined(value)) {
+        query.addAndQuery(`${columnName} BEGINSWITH "${value}"`);
+      }
+    }
+
+    if (conditions.has(`endsWith_${conditionPropKey}`)) {
+      const value = conditions.get(`endsWith_${conditionPropKey}`);
+
+      if (Common.isNotUndefined(value)) {
+        query.addAndQuery(`${columnName} ENDSWITH "${value}"`);
+      }
+    }
+
+    if (conditions.has(`contains_${conditionPropKey}`)) {
+      const value = conditions.get(`contains_${conditionPropKey}`);
+
+      if (Common.isNotUndefined(value)) {
+        query.addAndQuery(`${columnName} CONTAINS "${value}"`);
+      }
+    }
+
+    if (conditions.has(`contains_${conditionPropKey}s`)) {
+      const values = conditions.get(`contains_${conditionPropKey}s`);
+
+      if (Common.isNotUndefined(values) && !values.isEmpty()) {
+        const innerQuery = values.reduce((reduction, value) => reduction.addAndQuery(`${columnName} = "${value}"`), new Query()).getQuery();
+
+        query.addAndQuery(`(${innerQuery})`);
+      }
+    }
+
+    return query;
+  };
+
+  static addEqualToQuery = (conditions, query, conditionPropKey, columnName) => {
+    if (conditions.has(conditionPropKey)) {
+      const value = conditions.get(conditionPropKey);
+
+      if (Common.isNotUndefined(value)) {
+        query.addAndQuery(`${columnName} = "${value}"`);
+      }
+    }
+
+    return query;
+  };
+
+  static addNotEqualToQuery = (conditions, query, conditionPropKey, columnName) => {
+    if (conditions.has(`notEqual_${conditionPropKey}`)) {
+      const value = conditions.get(`notEqual_${conditionPropKey}`);
+
+      if (Common.isNotUndefined(value)) {
+        query.addAndQuery(`${columnName} <> "${value}"`);
+      }
+    }
+
+    return query;
+  };
+
+  static addLessThanToQuery = (conditions, query, conditionPropKey, columnName) => {
+    if (conditions.has(`lessThan_${conditionPropKey}`)) {
+      const value = conditions.get(`lessThan_${conditionPropKey}`);
+
+      if (Common.isNotUndefined(value)) {
+        query.lessThan(columnName, value);
+      }
+    }
+
+    return query;
+  };
+
+  static addLessThanOrEqualToQuery = (conditions, query, conditionPropKey, columnName) => {
+    if (conditions.has(`lessThanOrEqualTo_${conditionPropKey}`)) {
+      const value = conditions.get(`lessThanOrEqualTo_${conditionPropKey}`);
+
+      if (Common.isNotUndefined(value)) {
+        query.lessThanOrEqualTo(columnName, value);
+      }
+    }
+
+    return query;
+  };
+
+  static addGreaterThanToQuery = (conditions, query, conditionPropKey, columnName) => {
+    if (conditions.has(`greaterThan_${conditionPropKey}`)) {
+      const value = conditions.get(`greaterThan_${conditionPropKey}`);
+
+      if (Common.isNotUndefined(value)) {
+        query.greaterThan(columnName, value);
+      }
+    }
+
+    return query;
+  };
+
+  static addGreaterThanOrEqualToQuery = (conditions, query, conditionPropKey, columnName) => {
+    if (conditions.has(`greaterThanOrEqualTo_${conditionPropKey}`)) {
+      const value = conditions.get(`greaterThanOrEqualTo_${conditionPropKey}`);
+
+      if (Common.isNotUndefined(value)) {
+        query.greaterThanOrEqualTo(columnName, value);
       }
     }
 
